791f064c92502306c3bd55842f2116e9035a847d
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-06 22:00:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,20 @@ end
 def assign_rooms(attendees)
 attendees.each_with_index.map do |attendee, index|
   attendees[attendee]= index
-  "Hello, #{attendee}! You'll be assigned to room #{index.to_i+1}!"
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
+
+def printer(attendees)
+  batch_badge_creator.each do |badge|
+    puts badge
+  end
+
+  assign_rooms.each do |assignment|
+    puts assignment
+  end
+end
+
+
+
+
