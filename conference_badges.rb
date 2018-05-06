fef18fd0adb78a6309c9684a8f84236366a41e22
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-06 21:39:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 
 def assign_rooms(attendees)
-attendees.each_with_index do |attendee, index.to_i|
+attendees.each_with_index do |attendee, index|
   attendees[attendee]= index.to_i
   "Hello, #{attendee}! You'll be assigned to room #{index.to_i}!"
 end
